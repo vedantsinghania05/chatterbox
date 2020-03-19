c437fc80e68688a4b5f43fee02619c9cfedffde5
Dhruv Mittal <dhruv@Dhruvs-MacBook-Air.local>
2020-03-18 18:03:12 -0700

    last

--- nodeserver/src/api/group/controller.js
+++ nodeserver/src/api/group/controller.js
@@ -1,14 +1,27 @@
 import { resInternal, resOk, resNotFound, resNoContent, resCreated } from '../../services/response/'
 import { Group } from '.'
+import { User } from '../user'
 
 export const create = ({ body }, res, next) => {
-    let fields = { title: body.title, members: body.members };
-  
-    return Group.create(fields)
-      .then(group => {
-        if (!group) return next(resInternal('Failed to create group'));
-        return resCreated(res, group.view(true) )})
-      .catch(next)
+  let fields = { title: body.title, members: body.members };
+
+  User.find({ email: { $in: fields.members }})
+    .then(users => {
+      if (!users) return next(resInternal('Failed to find users'));
+      console.log('vvvvvvvvvvvvvvvvvvvvvvv')
+      console.log(users)
+      console.log('^^^^^^^^^^^^^^^^^^^^^^^')
+      let userIds = users.map(u => u.id)
+
+      fields.members = userIds
+
+      return Group.create(fields)
+        .then(group => {
+          if (!group) return next(resInternal('Failed to create group'));
+          return resCreated(res, group.view(true) )})
+        .catch(next)
+    })
+
 }
 
 export const index = ({ querymen: { query, select, cursor } }, res, next) =>
